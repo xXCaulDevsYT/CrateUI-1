862e07dadf286b2f1ca4b738e0b96ba9954d0b84
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-10-25 14:04:26 -0500

    Depends on Core

--- src/CrateUI/Commands/getkey.php
+++ src/CrateUI/Commands/getkey.php
@@ -21,7 +21,7 @@ class getkey extends Command{
        );
 	$this->setDescription("get crate key");
 	$this->setAliases(["key"]);
-	$this->setPermission("crate.key");
+	$this->setPermission("elixir.crate.key");
     }
 
     public function execute(CommandSender $sender, string $label, array $args){
@@ -36,13 +36,13 @@ class getkey extends Command{
         $legendaryname = Item::get(131,5,1);
         $e = Enchantment::getEnchantment(0);
         if (count($args) < 1){
-            $sender->sendMessage("§b===>§eKeys§b<===");
-            $sender->sendMessage("§a/getkey Common §e: §bGet Common key.");
-            $sender->sendMessage("§c/getkey Vote §e: §bGet Vote key.");
-            $sender->sendMessage("§6/getkey Rare §e: §bGet Rare key.");
-            $sender->sendMessage("§6/getkey Mythic §e: §bGet Mythic key.");
-            $sender->sendMessage("§b/getkey Legendary §9: §bGet Legendary key.");
-            $sender->sendMessage("§b===>§eKeys§b<===");
+            $sender->sendMessage("§7======== §l§dELIXIR§b CRATES§r§7 ========");
+            $sender->sendMessage("§cUsage: §f/getkey §e{keyname}");
+            $sender->sendMessage("§2Crates: §aCommon, Vote, Rare, Mythic, Legendary");
+            $sender->sendMessage("§7======== §l§6REDEEMING  HELP§r§7 ========");
+            $sender->sendMessage("§cUsage: §f/crate§r");
+            $sender->sendMessage("§2Description: §aCrate Menu Command§r");
+            $sender->sendMessage("§7======== §l§dELIXIR§b CRATES§r§7 ========");
             return false;
         }
         switch ($args[0]){
@@ -51,45 +51,45 @@ class getkey extends Command{
             case "Common":
             $e->setLevel(-1);
             $commonname->addEnchantment($e);
-            $commonname->setCustomName("§aCommon");
+            $commonname->setCustomName("§l§7COMMON§r§f Key");
             $inv->addItem($commonname);
-            $sender->sendMessage("§eYou receive §aCommon §eKey.");
+            $sender->sendMessage("§bYou Recieved a §l§7COMMON§r§b Crate Key!");
             break;
             case "2":
             case "vote":
             case "Vote":
             $e->setLevel(-1);
             $votename->addEnchantment($e);
-            $votename->setCustomName("§cVote");
+            $votename->setCustomName("§l§9VOTE§r§f Key");
             $inv->addItem($votename);
-            $sender->sendMessage("§eYou receive §cVote §eKey.");
+            $sender->sendMessage("§bYou Recieved a §l§9VOTE§r§b Crate Key!");
             break;
             case "3":
             case "rare":
             case "Rare":
             $e->setLevel(-1);
             $rarename->addEnchantment($e);
-            $rarename->setCustomName("§6Rare");
+            $rarename->setCustomName("§l§4RARE§r§f Key");
             $inv->addItem($rarename);
-            $sender->sendMessage("§eYou receive §6Rare §eKey.");
+            $sender->sendMessage("§bYou Recieved a §l§4RARE§r§b Crate Key!");
             break;
             case "4":
             case "mythic":
             case "Mythic":
             $e->setLevel(-1);
             $mythicname->addEnchantment($e);
-            $mythicname->setCustomName("§5Mythic");
+            $mythicname->setCustomName("§l§dMYTHIC§r§f Key");
             $inv->addItem($mythicname);
-            $sender->sendMessage("§eYou receive §5Mythic §eKey.");
+            $sender->sendMessage("§bYou Recieved a §l§dMYTHIC§r§b Crate Key!");
             break;
             case "5":
             case "legendary":
             case "Legendary":
             $e->setLevel(-1);
             $legendaryname->addEnchantment($e);
-            $legendaryname->setCustomName("§9Legendary");
+            $legendaryname->setCustomName("§a§lLEGENDARY§r§f Key");
             $inv->addItem($legendaryname);
-            $sender->sendMessage("§eYou receive §9Legendary §eKey.");
+            $sender->sendMessage("§bYou Recived a §l§aLEGENDARY§r§b Crate Key!");
         }
     }
 }
